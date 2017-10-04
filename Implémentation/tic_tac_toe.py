90a8b5fadeaec335a124ba0ac2b2663e8756a3f0
Nico Salamone <nico.salamone2411@yahoo.com>
2017-10-04 20:33:40 +0200

    Generation all board (explore whole tree)

--- Implémentation/tic_tac_toe.py
+++ Implémentation/tic_tac_toe.py
@@ -1,11 +1,20 @@
 import random
+import copy
+
+X_PLAYER = "x"
+O_PLAYER = "o"
+NONE_PLAYER = "." # If draw
+EMPTY_SQUARE = "-"
 
 SIZE = 3
 
-def alternate_players():
-	while True:
-		yield "x"
-		yield "o"
+def alternate_players(player):
+	if player == X_PLAYER:
+		return O_PLAYER
+	if player == O_PLAYER:
+		return X_PLAYER
+
+	return None
 
 def print_board(board):
 	for i in range(len(board)):
@@ -13,83 +22,121 @@ def print_board(board):
 			print(board[i][j], end = "")
 		print("")
 
+def get_position_from_num_square(num_square):
+	return (num_square // SIZE, num_square % SIZE)
+
 def play_random_ai(player, board, empty_squares):
 	if not empty_squares:
 		return False
 
 	num_square = random.randint(0, len(empty_squares) - 1)
-
-	board[(num_square // SIZE)][(num_square % SIZE)] = player
+	pos = get_position_from_num_square(empty_squares.pop(num_square))
+	board[pos[0]][pos[1]] = player
 
 	return True
 
-def end_party(board):
+def check_end_party(board):
 	# Columns
 	for j in range(SIZE):
 		player = board[0][j]
-		if player != "-":
+		if player != EMPTY_SQUARE:
 			i = 1
 			while (i < SIZE) and (board[i][j] == player):
 				i += 1
 			if i == SIZE:
-				print("Winner:", player, "(columns)")
 				return player
 
 	# Rows
 	for i in range(SIZE):
 		player = board[i][0]
-		if player != "-":
+		if player != EMPTY_SQUARE:
 			j = 1
 			while (j < SIZE) and (board[i][j] == player):
 				j += 1
 			if j == SIZE:
-				print("Winner:", player, "(rows)")
 				return player
 
 	# Diagonals
 	player = board[0][0]
-	if player != "-":
+	if player != EMPTY_SQUARE:
 		i = 1
 		j = 1
 		while (i < SIZE) and (j < SIZE) and (board[i][j] == player):
 			i += 1
 			j += 1
 		if (i == SIZE) and (j == SIZE):
-			print("Winner:", player, "(diagonals)")
 			return player
 
 	# Anti-diagonals
 	player = board[SIZE - 1][0]
-	if player != "-":
+	if player != EMPTY_SQUARE:
 		i = SIZE - 2
 		j = 1
 		while (i >= 0) and (j < SIZE) and (board[i][j] == player):
 			i -= 1
 			j += 1
 		if (i == -1) and (j == SIZE):
-			print("Winner:", player, "(anti-diagonals)")
 			return player
 
-	return None
+	for i in range(SIZE):
+		for j in range(SIZE):
+			if board[i][j] == EMPTY_SQUARE:
+				return False
 
-# "x": X player
-# "o": O player
-# "–": empty box
-players = alternate_players()
-board = [["-" for i in range(SIZE)] for i in range(SIZE)]
-#empty_squares = [(i, j) for i in range(SIZE) for j in range(SIZE)]
-empty_squares = [i for i in range((SIZE * SIZE))]
+	return NONE_PLAYER
 
-continue_game = True
-print_board(board)
-while continue_game:
-	input("")
-	current_player = next(players)
-	play_random_ai(current_player, board, empty_squares)
+def generate_all_board(player = X_PLAYER):
+	# Score determinated by player
+	# score = [w, l, d], where 'w' is the number of victories, 'l' is the number of defeats and 'd' is the numbers of draws of 'player'
+	board = [[EMPTY_SQUARE for i in range(SIZE)] for i in range(SIZE)]
+	empty_squares = [i for i in range((SIZE * SIZE))]
+
+	current_player = X_PLAYER
+
+	generate_all_board_rec(player, board, empty_squares, current_player)
+
+def generate_all_board_rec(player, board, empty_squares, current_player):
+	score = [0] * 3
+
+	winning_player = check_end_party(board)
+	if(not winning_player):
+		for i in range(len(empty_squares)):
+			board_temp = copy.deepcopy(board)
+			empty_squares_temp = copy.copy(empty_squares)
+
+			pos = get_position_from_num_square(empty_squares_temp.pop(i))
+			board_temp[pos[0]][pos[1]] = current_player
+
+			score_temp = generate_all_board_rec(player, board_temp, empty_squares_temp, alternate_players(current_player))
+			score = [(score[i] + score_temp[i]) for i in range(len(score))]
+	elif(winning_player == NONE_PLAYER): # Draw
+		score[2] += 1
+	elif(winning_player == player): # Win
+		score[0] += 1
+	else: # Lose
+		score[1] += 1
+
 	print_board(board)
-	if end_party(board) or not empty_squares:
-		continue_game = False
+	print(score)
+	print()
 
-#board = [["x", "x", "o"], ["o", "o", "x"], ["o", "o", "x"]]
-#print_board(board)
-#end_party(board)
+	return score
+
+if __name__ == "__main__":
+	generate_all_board()
+
+	"""
+	board = [["-" for i in range(SIZE)] for i in range(SIZE)]
+	empty_squares = [i for i in range((SIZE * SIZE))]
+
+	current_player = X_PLAYER
+	continue_game = True
+	print_board(board)
+	while continue_game:
+		input("")
+		play_random_ai(current_player, board, empty_squares)
+		current_player = alternate_players(current_player)
+		print_board(board)
+		if check_end_party(board):
+			continue_game = False
+	"""
