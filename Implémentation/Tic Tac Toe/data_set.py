344b388f698b7a8a5933c4ce788a3299aa41079e
Nico Salamone <nico.salamone2411@yahoo.com>
2017-11-23 21:00:31 +0100

    Code refactoring

--- Implémentation/Tic Tac Toe/data_set.py
+++ Implémentation/Tic Tac Toe/data_set.py
@@ -6,19 +6,18 @@ import tic_tac_toe as ttt
 @lru_cache(maxsize = None)
 def generate_data_set(grid = ttt.initialize_empty_grid()):
 	state = ttt.get_grid_state(grid)
-	player = ttt.get_player_who_must_play_now(grid)
-	other_player = ttt.Player.get_other_player(player)
 
 	if state == ttt.State.WIN:
-		return [(grid, (1, 0, 0), other_player)]
+		return [(grid, (1, 0, 0))]
 	elif state == ttt.State.LOSE:
-		return [(grid, (0, 1, 0), other_player)]
+		return [(grid, (0, 1, 0))]
 	elif state == ttt.State.DRAW:
-		return [(grid, (0, 0, 1), other_player)]
+		return [(grid, (0, 0, 1))]
 	else: # If the state is continue.
 		tree = []
 		scores = (0, 0, 0)
 
+		player = ttt.get_player_who_must_play_now(grid)
 		empty_tile_indexes = ttt.get_empty_tile_indexes(grid)
 		for tile_index in empty_tile_indexes:
 			subtree = generate_data_set(ttt.play_turn(grid, player, tile_index))
@@ -27,7 +26,7 @@ def generate_data_set(grid = ttt.initialize_empty_grid()):
 			tree.extend(subtree)
 			scores = tuple(map(lambda x, y: x + y, scores, subtree_scores))
 
-		tree.insert(0, (grid, scores, other_player))
+		tree.insert(0, (grid, scores))
 
 	return tree
 
@@ -45,10 +44,9 @@ def write_data_set(data_set, data_set_file_name):
 	with open(data_set_file_name, 'w', newline = '') as data_set_file:
 		csv_writer = csv.writer(data_set_file, delimiter = ';', quotechar = '\"', quoting = csv.QUOTE_MINIMAL)
 
-		for grid, scores, player in data_set:
+		for grid, scores in data_set:
 			row = [tile.value for tile in grid]
 			row.extend([score for score in scores])
-			row.append(player.value)
 
 			csv_writer.writerow(row)
 
@@ -62,9 +60,8 @@ def read_data_set(data_set_file_name):
 			tiles = row[:n]
 			grid = tuple(ttt.Tile(int(tile)) for tile in tiles)
 			scores = tuple(map(int, row[n : n + 3]))
-			player = ttt.Player(int(row[n + 3]))
 
-			data_set.append((grid, scores, player))
+			data_set.append((grid, scores))
 
 	return data_set
 
@@ -78,8 +75,7 @@ if __name__ == "__main__":
 	data_set = read_data_set(data_set_file_name)
 
 	#"""
-	for grid, scores, player in data_set:
+	for grid, scores in data_set:
 		ttt.print_grid(grid)
-		print(scores)
-		print(player, "\n")
+		print(scores, "\n")
 	#"""
--- Implémentation/Tic Tac Toe/neural_network.py
+++ Implémentation/Tic Tac Toe/neural_network.py
@@ -9,7 +9,66 @@ from keras.models import load_model
 import tic_tac_toe as ttt
 import data_set as ds
 
-def get_win_ratio(scores):
+def get_data_for_neural_network(training_set, y_true_computation_func):
+	# Get data (x, y_true) for training the neural network.
+	# x is the grid and y is a value compted by y_true_computation_func.
+	# For each x, y_true is computed by sending x to the function y_true_computation_func.
+	# This function has two parameters: the grid and the scores.
+	
+	n = len(training_set)
+	
+	x = [0] * n
+	y_true = [0] * n
+	for i in range(n):
+		grid = training_set[i][0]
+		scores = training_set[i][1]
+
+		x[i] = tuple(tile.value for tile in grid) # Grid.
+		y_true[i] = y_true_computation_func(grid, scores)
+
+	return x, y_true
+
+def create_fit_model(x, y_true, output_size = None, model_file_name = None):
+	if output_size == None:
+		if hasattr(y_true[0], "__len__"):
+			output_size = len(y_true[0])
+		else:
+			output_size = 1
+
+	model = Sequential()
+	model.add(Dense(15, input_dim = 9))
+	model.add(Dense(29))
+	model.add(Dense(output_size))
+
+	model.compile(loss = 'mean_squared_error', optimizer = 'sgd', metrics = ['mean_squared_error', 'mean_absolute_error'])
+
+	#model.fit(x, y_true, epochs = 100, batch_size = 1)
+	model.fit(x, y_true, epochs = 10, batch_size = 100)
+	if model_file_name:
+		model.save(model_file_name)
+
+	return model
+
+def print_loss_metric_functions(model, x, y_true):
+	data_evaluation = model.evaluate(x, y_true, batch_size = 1)
+
+	print("\n\n")
+	
+	for i in range(len(data_evaluation)):
+		print("{}: {:.2f}".format(model.metrics_names[i], data_evaluation[i]))
+
+def print_neural_network_results(errors, error_names = None):
+	# The errors variable is a list of the errors.
+	# For example, the errors variable contains a list of the absolute errors and a list of the relative errors.
+	
+	num_errors = len(errors)
+
+	if error_names == None:
+		error_names = [""] * num_errors
+		for i in range(num_errors):
+			error_names[i] = "Error #{}".format(i + 1)
+
+def get_win_ratio(grid, scores):
 	num_wins = scores[0]
 	num_losses = scores[1]
 	num_draws = scores[2]
@@ -32,9 +91,11 @@ def error_bins_data_percentage(errors, num_bins = 10, value_range = None):
 
 	return (data_percentages, bins)
 
-def get_grid_indexes_in_error_range(grids, errors, value_range):
-	# Get grid and error indexes whose the error lies in the range.
-	# Each grid corresponds to an error (the i th grid corresponds to the i th error).
+def get_grid_in_error_range(grids, scores_true, scores_pred, errors, value_range = None):
+	# Get grid, score_true, score_pred and error when error lies in the range.
+	
+	if value_range == None:
+		value_range = (min(errors), max(errors))
 
 	min_value = value_range[0]
 	max_value = value_range[1]
@@ -42,7 +103,7 @@ def get_grid_indexes_in_error_range(grids, errors, value_range):
 	grids_in_range = []
 	for i in range(len(errors)):
 		if min_value <= errors[i] <= max_value:
-			grids_in_range.append(i)
+			grids_in_range.append((grids[i], scores_true[i], scores_pred[i], errors[i]))
 
 	return grids_in_range
 
@@ -57,46 +118,24 @@ if __name__ == "__main__":
 	np.random.seed(int(seed))
 
 	data_set = ds.read_data_set(data_set_file_name)
-	training_set = random.sample(data_set, len(data_set))
+	#training_set = random.sample(data_set, num_samples)
+	training_set = data_set
+	x, y_true = get_data_for_neural_network(training_set, get_win_ratio)
+	model = create_fit_model(x, y_true)
 
-	x = [0] * len(training_set)
-	y_true = [0] * len(training_set)
-	for i in range(len(training_set)):
-		grid = training_set[i][0]
-		scores = training_set[i][1]
+	# Results
 
-		x[i] = tuple(tile.value for tile in grid) # Grid.
-		y_true[i] = get_win_ratio(scores) # Ratio of wins.
+	print_loss_metric_functions(model, x, y_true)
 
-	model = Sequential()
-	model.add(Dense(15, input_dim = 9))
-	model.add(Dense(29))
-	model.add(Dense(1))
-
-	model.compile(loss = 'mean_squared_error', optimizer = 'sgd', metrics = ['mean_squared_error', 'mean_absolute_error'])
-
-	#model.fit(x, y_true, epochs = 100, batch_size = 1)
-	model.fit(x, y_true, epochs = 10, batch_size = 100)
-	model.save(model_file_name)
-
-	data_evaluation = model.evaluate(x, y_true)
 	y_pred = model.predict(x)
-
-	# Mean squared error.
-	print("\n\n{}:".format(model.metrics_names[1]))
-	print("{:.2%}".format(data_evaluation[1]))
-
-	# Mean absolute error.
-	print("\n{}:".format(model.metrics_names[2]))
-	print("{:.2%}".format(data_evaluation[2]))
+	errors = list(map(lambda y_t, y_p: abs(y_t - y_p), y_true, y_pred.flatten().tolist()))
 
 	# Correlation coefficient and p-value.
 	print("\nCorrelation coefficient and p-value:")
 	print(pearsonr(y_true, y_pred.flatten()))
 
 	# Error bins.
-	print("\nError bins:")
-	errors = list(map(lambda y_t, y_p: abs(y_t - y_p), y_true, y_pred.flatten().tolist()))
+	print("\nThe percentage of data which are in the follow bins:")
 	data_percentages, bins = error_bins_data_percentage(errors, 10, (0.0, 1.0))
 	for i in range(len(data_percentages)):
 		print("[{:.2f}-{:.2f}]: {:.3%}".format(bins[i], bins[i+1], data_percentages[i]))
@@ -107,21 +146,21 @@ if __name__ == "__main__":
 	for i in range(len(x)):
 		grid = tuple(ttt.Tile(tile) for tile in x[i])
 
-		print()
 		ttt.print_grid(grid)
 		print("Error: {:.2f}".format(errors[i]))
 		print("y_true: {:.2f}".format(y_true[i]))
 		print("y_pred: {:.2f}".format(y_pred[i][0]))
+		print()
 	"""
 
 	# Grids with a bad prediction
 	print("\nGrids with a bad prediction:")
-	grids_with_bad_prediction_indexex = get_grid_indexes_in_error_range(x, errors, (0.5, 1.0))
-	for i in grids_with_bad_prediction_indexex:
-		grid = tuple(ttt.Tile(tile) for tile in x[i])
+	grids_with_bad_prediction = get_grid_in_error_range(x, y_true, y_pred, errors, (0.5, 1.0))
+	for x_gr, y_t, y_p, err in grids_with_bad_prediction:
+		grid = tuple(ttt.Tile(tile) for tile in x_gr)
 
-		print()
 		ttt.print_grid(grid)
-		print("Error: {:.2f}".format(errors[i]))
-		print("y_true: {:.2f}".format(y_true[i]))
-		print("y_pred: {:.2f}".format(y_pred[i][0]))
+		print("Error: {:.2f}".format(err))
+		print("y_true: {:.2f}".format(y_t))
+		print("y_pred: {:.2f}".format(y_p[0]))
+		print()
