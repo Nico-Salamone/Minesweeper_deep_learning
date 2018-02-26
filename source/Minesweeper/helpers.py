e5fc7534fb726f71d8205a01f02f8c5f370d12b3
Nico Salamone <nico.salamone2411@yahoo.com>
2018-02-26 17:44:33 +0100

    Add mask generation

--- source/Minesweeper/helpers.py
+++ source/Minesweeper/helpers.py
@@ -1,4 +1,9 @@
+from minesweeper.grid import Tile, get_positions
 from minesweeper.masked_grid import MaskedTile
+
+import random
+import math
+import copy
 
 def to_value_list(grid):
 	"""
@@ -20,6 +25,85 @@ def to_value_list(grid):
 			value_list.append(tile)
 
 	return value_list
+
+def compute_walls(grid):
+	"""
+	Compute the thickness of walls from a grid.
+
+	:grid: A grid (a list of tile values, that is an one-dimensional grid).
+	:return: the thickness of walls.
+	"""
+
+	num_tiles = len(grid)
+	edge_size = int(math.sqrt(num_tiles))
+	radius = int((edge_size - 1) / 2)
+
+	left_wall = 0
+	right_wall = 0
+	top_wall = 0
+	bottom_wall = 0
+
+	for i in range((radius * edge_size), ((radius * edge_size) + radius)):
+		if grid[i] == Tile.WALL:
+			left_wall += 1
+
+	for i in range(((radius * edge_size) + radius + 1), ((radius + 1) * edge_size)):
+		if grid[i] == Tile.WALL:
+			right_wall += 1
+
+	for i in range(radius, (radius * edge_size), edge_size):
+		if grid[i] == Tile.WALL:
+			top_wall += 1
+
+	for i in range(((radius + 1) * edge_size) + radius, num_tiles, edge_size):
+		if grid[i] == Tile.WALL:
+			bottom_wall += 1
+
+	return (left_wall, right_wall, top_wall, bottom_wall)
+
+def generate_random_mask(subgrid, min_masked_tiles=0, max_masked_tiles=None):
+	"""
+	Generate a list of grids with a mask.
+	The tile in the middle of 'subgrid' is masked and this tile is added to a list. Then, this function rands a number 'n'
+	between 'min_masked_tiles' and 'max_masked_tiles'. Then, it masks a tile randomly and adds it to the list (2 tiles are
+	masked). Then, it masks again a tile randomly and adds it to the list (3 tiles is masked). It repeats this until 'n' + 1
+	tiles are masked. If 'n' is greater than the the number of 'availabe' tiles (tiles that can be masked), then 'n' is
+	replaced by this number.
+	The wall tiles are not masked.
+
+	:subgrid: The subgrid (a list of tile values, that is an one-dimensional grid).
+	:min_masked_tiles: The minimum number of tiles to mask.
+	:max_masked_tiles: The maximum number of tiles to mask.
+	:return: A list of grids with a mask (a list of tile values, that is an one-dimensional grid).
+	"""
+
+	num_tiles = len(subgrid)
+	edge_size = int(math.sqrt(num_tiles))
+	radius = int((edge_size - 1) / 2)
+	left_wall, right_wall, top_wall, bottom_wall = compute_walls(subgrid)
+
+	if max_masked_tiles == None:
+		max_masked_tiles = num_tiles - 1 # The tile in the middle of the grid can not be masked.
+
+	pos = get_positions(edge_size, edge_size, left_wall, right_wall, top_wall, bottom_wall)
+	pos.remove((radius, radius)) # Tile in the middle of the grid.
+
+	num_masked_tiles = random.randint(min_masked_tiles, max_masked_tiles)
+	if num_masked_tiles > len(pos):
+		num_masked_tiles = len(pos)
+
+	masked_tiles_pos = [(radius, radius)]
+	masked_tiles_pos.extend(random.sample(pos, num_masked_tiles))
+
+	masked_subgrids = []
+	for i, j in masked_tiles_pos:
+		k = (i * edge_size) + j
+
+		subgrid = copy.copy(subgrid)
+		subgrid[k] = MaskedTile.MASKED.value
+		masked_subgrids.append(subgrid)
+
+	return masked_subgrids
 
 def extract_subgrid(grid, i, j, radius):
 	"""
@@ -48,7 +132,7 @@ def extract_subgrid(grid, i, j, radius):
 	return subgrid
 
 if __name__ == "__main__":
-	from masked_grid import MaskedGrid
+	from minesweeper.masked_grid import MaskedGrid
 	bomb_position_list = [(5, 4), (4, 2), (2, 1), (4, 4)]
 	g = MaskedGrid(10, 5, bomb_position_list, 1, 0, 2, 3)
 	g.unmask_tile(2, 4)
@@ -61,4 +145,23 @@ if __name__ == "__main__":
 	for row in sg:
 		for tile in row:
 			print(tile, end='\t')
+		print('')
+
+
+
+	print('\n\n')
+	from minesweeper.grid_generation import generate_subgrid
+	radius = 2
+	edge_size = (radius * 2) + 1
+
+	g = generate_subgrid(radius, 10, 10, 10)
+	g2 = to_value_list(g)
+
+	print(g)
+	print(g2)
+	for msg in generate_random_mask(g2, 3, 16):
+		for i in range(len(msg)):
+			print(msg[i], end='\t')
+			if (i > 0) and ((i + 1) % edge_size == 0):
+				print('')
 		print('')
