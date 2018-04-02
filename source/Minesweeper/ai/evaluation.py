a1bbc10bd0204984479c46fb809834c0af21f6ca
Nico Salamone <nico.salamone2411@yahoo.com>
2018-04-02 13:30:38 +0200

    Add ai evaluation

---
+++ source/Minesweeper/ai/evaluation.py
@@ -0,0 +1,67 @@
+from minesweeper.minesweeper import Minesweeper, State
+
+import random
+
+def scores(ai, num_games, num_rows_grid, num_columns_grid, num_bombs_grid, first_round_random=False):
+	"""
+	Get the scores of an artificial intelligence. This function creates 'num_games' games and the artificial
+	intelligence plays on them.
+
+	:ai: An artificial intelligence.
+	:num_games: The number of games where the artificial intelligence plays on them.
+	:num_rows_grid: The number of rows of the original grid.
+	:num_columns_grid: The number of columns of the original grid.
+	:num_bombs_grid: The number of bombs of the grid.
+	:first_round_random: If True, then the first round of each game is random. If False, then the first round of each
+		game is determined by the artificial intelligence.
+	:return: The scores of the artificial intelligence.
+	"""
+
+	score_list = []
+	for i in range(num_games):
+		# It is not possible to lose to the first round.
+		state = State.LOSS
+		while state == State.LOSS:
+			ms = Minesweeper(num_rows_grid, num_columns_grid, num_bombs_grid)
+
+			if first_round_random:
+				pos = (random.randint(0, (num_rows_grid - 1)), random.randint(0, (num_columns_grid - 1)))
+			else:
+				pos = ai.best_tile_pos(ms.grid)
+
+			ms.play_tile(pos[0], pos[1])
+			state = ms.state
+
+		while ms.state == State.CONTINUE:
+			pos = ai.best_tile_pos(ms.grid)
+			ms.play_tile(pos[0], pos[1])
+
+		score_list.append(ms.score)
+
+	return score_list
+
+if __name__ == "__main__":
+	from ai.ai import AI
+	from ai.helpers import model_file_path
+
+	from keras.models import load_model
+	import numpy as np
+
+	random.seed(42)
+
+	num_games = 1000
+	num_rows_grid = 10
+	num_columns_grid = 10
+	num_bombs_grid = 10
+	subgrid_radius = 2
+
+	model_file_name = model_file_path(num_rows_grid, num_columns_grid, num_bombs_grid, subgrid_radius)
+	model = load_model(model_file_name)
+
+	ai = AI(model, subgrid_radius)
+
+	score_list = scores(ai, num_games, num_rows_grid, num_columns_grid, num_bombs_grid, first_round_random=True)
+
+	print("Min: {}\nMax: {}\nMean: {:.3f}\nPercentile 25: {}\nPercentile 50 (median): {}\nPercentile 75: {}".format(
+		min(score_list), max(score_list), np.mean(score_list), np.percentile(score_list, 25),
+		np.percentile(score_list, 50), np.percentile(score_list, 75)))
